a014906e0cc5deb917a35e251bee059a2e57dd2e
Aaron Lewis <aarondev8917@gmail.com>
2020-12-10 13:51:07 +0530

    Endpoint 1: Get list of providers

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Resources\ProviderCollection;
+use App\Models\Provider;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +19,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/providers',  function () {
+    return new ProviderCollection(Provider::all());   
 });
