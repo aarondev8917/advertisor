c9f8fd3a634f7b7f9d00a40a239881f5e62e9d1a
Aaron Lewis <aarondev8917@gmail.com>
2020-12-10 20:54:57 +0530

    minor changes to startup scripts

--- start.sh
+++ start.sh
@@ -2,5 +2,9 @@
 
 sudo apt update
 sh install-docker.sh
+sudo service docker start
 alias sail='bash vendor/bin/sail'
 sail up -d
+sail php artisan migrate
+sail php artisan db:seed
+sail php artisan storage:link
